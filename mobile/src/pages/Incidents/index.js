3dcd05f2ed0fa6dea25e5d3502ad0216f877cddc
Lucas Gonçalves <lucas@gosd.com.br>
2020-03-30 13:40:22 -0300

    Mobile developement, All OK

--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -11,14 +11,32 @@ import styles from './styles';
 export default function Incidents(){
     const navigation = useNavigation();
     const [incidents, setIncidents] = useState([]);
+    const [total, setTotal] = useState(0);
+    const [page, setPage] = useState(1);
+    const [loading, setLoading] = useState(false);
 
-    function navigateToDetail(){
-        navigation.navigate('Detail');
+    function navigateToDetail(incident){
+        navigation.navigate('Detail', { incident });
     }
 
     async function loadIncidents(){
-        const response = await api.get('incidents');
-        setIncidents(response.data);
+        if (loading){
+            return;
+        }
+
+        if(total > 0 && incidents.length == total){
+            return;
+        }
+
+        setLoading(true);
+
+        const response = await api.get('incidents', {
+            params: { page }
+        });
+        setIncidents([...incidents, ...response.data]);
+        setTotal(response.headers['x-total-count']);
+        setPage( page + 1);
+        setLoading(false);
     };
 
     useEffect(() => {
@@ -30,7 +48,7 @@ export default function Incidents(){
             <View style={styles.header}>
                 <Image source={logoImg} />
                 <Text style={styles.headerText}>
-                    Total de <Text style={styles.headerTextBold}>0 casos.</Text>
+                    Total de <Text style={styles.headerTextBold}>{total} casos.</Text>
                 </Text>
             </View>
 
@@ -44,6 +62,8 @@ export default function Incidents(){
                 data={incidents}
                 keyExtractor={incident => String(incident.id)}
                 showsVerticalScrollIndicator={false}
+                onEndReached={loadIncidents}
+                onEndReachedThreshold={0.2}
                 renderItem={({ item: incident }) => (
                     <View style={styles.incident}>
                     <Text style={[styles.incidentProperty, {marginTop: 0,}]}>ONG:</Text>
@@ -57,7 +77,7 @@ export default function Incidents(){
 
                     <TouchableOpacity style={styles.detailsButton} 
                     style={styles.detailsButton} 
-                    onPress={navigateToDetail}>
+                    onPress={() => navigateToDetail(incident)}>
 
                     <Text style={styles.detailsButtonText}>Ver mais detalhes</Text>
                     <Feather name="arrow-right" size={16} color="#E02041"></Feather>
